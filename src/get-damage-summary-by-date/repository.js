8e8e52bfbc0df369b3636e4af05726b4042a435c
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-02-04 09:51:08 -0300

    feat: implements repository

--- src/get-damage-summary-by-date/repository.js
+++ src/get-damage-summary-by-date/repository.js
@@ -1,5 +1,24 @@
+const { prisma } = require('../libs/prismaClient');
+
 module.exports = {
-    async execute() {
-        // TODO: acesso ao banco
-    },
+  async execute(dateStart, dateEnd) {
+    const dbAlerts = await prisma.alert.findMany({
+      where: {
+        date: {
+          gte: `${dateStart}T00:00:00Z`,
+          lte: `${dateEnd}T23:59:59Z`,
+        },
+      },
+      select: {
+        date: true,
+        damage: true,
+        event: true,
+      },
+      orderBy: {
+        date: 'desc',
+      },
+    });
+
+    return dbAlerts;
+  },
 };
