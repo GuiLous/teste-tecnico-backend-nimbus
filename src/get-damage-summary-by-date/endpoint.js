363be44369d6de97aa3bb622d8fdc1c45ea1e8c9
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-02-04 09:50:45 -0300

    feat: implements endpoint

--- src/get-damage-summary-by-date/endpoint.js
+++ src/get-damage-summary-by-date/endpoint.js
@@ -1,7 +1,22 @@
+const url = require('url');
 const controller = require('./controller');
 
 module.exports = {
-    execute() {
-        // TODO: tratamento da requisição e resposta do caso de uso
-    },
+  async execute(req, res) {
+    try {
+      const { query } = url.parse(req.url, true);
+      const { dateStart, dateEnd } = query;
+
+      try {
+        const damageSummary = await controller.execute(dateStart, dateEnd);
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(damageSummary));
+      } catch (error) {
+        throw error;
+      }
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+  },
 };
