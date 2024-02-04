4ecff4b929bbd2883e62d2fa239f1fec17b6a783
GuiLou <guilhermesilva@acad.ifma.edu.br>
2024-02-04 09:50:29 -0300

    feat: implements server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,22 @@ const http = require('http');
 
 const getDamageSummaryByDateRoute = require('./get-damage-summary-by-date/route');
 
-const server = http.Server();
+const server = http.createServer(async (req, res) => {
+  const isPathValid = req.url.startsWith(getDamageSummaryByDateRoute.path);
+  const isMethodValid =
+    req.method.toLocaleLowerCase() === getDamageSummaryByDateRoute.method;
+
+  if (isPathValid && isMethodValid) {
+    try {
+      return await getDamageSummaryByDateRoute.fn(req, res);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    }
+  }
+
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  return res.end('404 Not Found');
+});
 
 module.exports = server;
